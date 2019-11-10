34c45dee98faff9c484039f513247ce0e512a725
John Acocella <jacocella@hotmail.com>
2019-11-10 17:32:31 -0500

    created generic user api

---
+++ users-api/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,7 @@
+INSERT INTO users(user_name, first_name, last_name)
+VALUES ('quinto', 'timothy', 'quinto'),
+('darkness', 'charlie', 'murphy'),
+('hilldawg', 'hillary', 'clinton'),
+('onionfan', 'ser', 'davos'),
+('1337codr', 'mad', 'hax'),
+('badstash', 'charlie', 'chaplin');
